05e3a79c4c0b07dfa30ef1a94e086e72e53f3b03
kssampson <kylesampsonmusic@gmail.com>
2024-01-31 16:40:25 -0800

    started auth local strategy and guard, and find by username in user service

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -4,10 +4,15 @@ import { AuthController } from './auth.controller';
 import { UserService } from 'src/user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
+import { JwtService, JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './strategies/local-strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthService, UserService],
+  imports: [TypeOrmModule.forFeature([User]), JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions: {expiresIn: "3600s"}
+  })],
+  providers: [AuthService, UserService, JwtService, LocalStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
